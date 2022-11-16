81c7c05a2bea37dbe8b82ed6a2fed50c7c7f6632
Miguel Sanchez <mangelsr25@gmail.com>
2022-11-15 23:25:09 -0500

    [ADD] placeholderData property on useLabels hook [ADD] Comments to diferientiate initialData and placeholderData

--- src/issues/hooks/useLabels.tsx
+++ src/issues/hooks/useLabels.tsx
@@ -9,12 +9,34 @@ const getLabels = async (): Promise<Label[]> => {
   return data;
 };
 
+// initialData != placeholderData
+// placeholderData: data show meanwhile the first data fetch is working
+// initialData: considered as fresh data may cause problem if it is imcomplete
+
 export const useLabels = () => {
   const labelsQuery = useQuery(
     ['labels'],
     getLabels,
     {
-      staleTime: 1000 * 60 * 60
+      staleTime: 1000 * 60 * 60,
+      placeholderData: [
+        {
+          id: 725156255,
+          node_id: "MDU6TGFiZWw3MjUxNTYyNTU=",
+          url: "https://api.github.com/repos/facebook/react/labels/good%20first%20issue%20(taken)",
+          name: "good first issue (taken)",
+          color: "b60205",
+          default: false,
+        },
+        {
+          id: 717031390,
+          node_id: "MDU6TGFiZWw3MTcwMzEzOTA=",
+          url: "https://api.github.com/repos/facebook/react/labels/good%20first%20issue",
+          name: "good first issue",
+          color: "6ce26a",
+          default: true
+        },
+      ],
     },
   );
   return labelsQuery;
