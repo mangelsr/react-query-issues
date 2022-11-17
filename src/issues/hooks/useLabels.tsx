d034b9e600f97d8e9f2ef761ce3193526ef43c35
Miguel Sanchez <mangelsr25@gmail.com>
2022-11-16 22:20:47 -0500

    [ADD] Override headers auth on /labels request

--- src/issues/hooks/useLabels.tsx
+++ src/issues/hooks/useLabels.tsx
@@ -5,7 +5,11 @@ import { sleep } from '../../helpers/sleep';
 
 const getLabels = async (): Promise<Label[]> => {
   await sleep();
-  const { data } = await githubApi.get<Label[]>('/labels');
+  const { data } = await githubApi.get<Label[]>('/labels', {
+    headers: {
+      Authorization: null,
+    },
+  });
   return data;
 };
 
