1e2cc717c45f3c4217232c0e254500bc33cb0b4c
Miguel Sanchez <mangelsr25@gmail.com>
2022-11-17 12:01:39 -0500

    [ADD] Navigation on IssueItem component

--- src/issues/components/IssueItem.tsx
+++ src/issues/components/IssueItem.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react';
 import { FiInfo, FiMessageSquare, FiCheckCircle } from 'react-icons/fi';
+import { useNavigate } from 'react-router-dom';
 import { Issue, State } from '../interfaces';
 
 
@@ -8,8 +9,12 @@ interface Props {
 }
 
 export const IssueItem:FC<Props> = ({ issue }) => {
+    const navigate = useNavigate();
     return (
-        <div className="card mb-2 issue">
+        <div
+            className="card mb-2 issue"
+            onClick={() => navigate(`/issues/issue/${issue.number}`)}
+        >
             <div className="card-body d-flex align-items-center">
                 {
                     issue.state === State.Open
