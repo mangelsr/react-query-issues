42f8e15b9f91aa395102ed38e0d8218198324620
Miguel Sanchez <mangelsr25@gmail.com>
2022-11-16 23:00:43 -0500

    [ADD] Props and render received issue list

--- src/issues/components/IssueList.tsx
+++ src/issues/components/IssueList.tsx
@@ -1,6 +1,13 @@
+import { FC } from 'react';
 import { IssueItem } from './IssueItem';
+import { Issue } from '../interfaces/';
 
-export const IssueList = () => {
+
+interface Props {
+    issues: Issue[];
+};
+
+export const IssueList:FC<Props> = ({ issues }) => {
     return (
         <div className="card border-white">
             <div className="card-header bg-dark">
@@ -18,10 +25,12 @@ export const IssueList = () => {
             </div>
             <div className="card-body text-dark">
                 {
-                    [1,2,3].map( issue => (
-                        <IssueItem key={issue} />
+                    issues.map( issue => (
+                        <IssueItem
+                            key={issue.id}
+                            issue={issue}
+                        />
                     ))
-                
                 }                
             </div>
         </div>
