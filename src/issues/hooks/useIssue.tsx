9204847e8a40f171af7811e2799fbd443fa14cf8
Miguel Sanchez <mangelsr25@gmail.com>
2022-11-17 15:18:54 -0500

    [UPD] Export fetch functions of useIssue hook

--- src/issues/hooks/useIssue.tsx
+++ src/issues/hooks/useIssue.tsx
@@ -3,13 +3,13 @@ import { githubApi } from "../../api/githubApi";
 import { sleep } from "../../helpers/sleep";
 import { Issue } from "../interfaces";
 
-const getIssueInfo = async (issueNumber: number):Promise<Issue> => {
+export const getIssueInfo = async (issueNumber: number):Promise<Issue> => {
   sleep(2);
   const { data } = await githubApi.get<Issue>(`/issues/${issueNumber}`);
   return data;
 };
 
-const getIssueComments = async (issueNumber: number):Promise<Issue[]> => {
+export const getIssueComments = async (issueNumber: number):Promise<Issue[]> => {
   sleep(2);
   const { data } = await githubApi.get<Issue[]>(`/issues/${issueNumber}/comments`);
   return data;
