7b0a02bff5917934c7d5a3501076e41cca1aa3d1
Miguel Sanchez <mangelsr25@gmail.com>
2022-11-16 23:00:06 -0500

    [ADD] useIssues hook on ListView component

--- src/issues/views/ListView.tsx
+++ src/issues/views/ListView.tsx
@@ -1,12 +1,15 @@
 import { useState } from 'react';
+import { LoadingIcon } from '../../shared/components/LoadingIcon';
 
 import { IssueList } from '../components/IssueList';
 import { LabelPicker } from '../components/LabelPicker';
+import { useIssues } from '../hooks';
 
 
 export const ListView = () => {
   
   const [selectedLabels, setSelectedLabels] = useState<string[]>([]);
+  const { issuesQuery } = useIssues();
   
   const onLabelChanged = (labelName: string) => {
     selectedLabels.includes(labelName)
@@ -18,7 +21,11 @@ export const ListView = () => {
     <div className="row mt-5">
       
       <div className="col-8">
-        <IssueList />
+        { 
+          issuesQuery.isLoading
+            ? <LoadingIcon/>
+            : <IssueList issues={ issuesQuery.data || [] } />
+        }
       </div>
       
       <div className="col-4">
