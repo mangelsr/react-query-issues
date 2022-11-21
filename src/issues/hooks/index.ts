70cf1cf6f3826eedc8f4df5736641bf4d48017eb
Miguel Sanchez <mangelsr25@gmail.com>
2022-11-21 15:33:12 -0500

    [ADD] useIssuesInfinite hook [ADD] added to hooks barel file

--- src/issues/hooks/index.ts
+++ src/issues/hooks/index.ts
@@ -1,3 +1,4 @@
 export { useLabels } from './useLabels';
 export { useIssue, getIssueComments, getIssueInfo } from './useIssue';
 export { useIssues } from './useIssues';
+export { useIssuesInfinite } from './useIssuesInfinite';
---
+++ src/issues/hooks/useIssuesInfinite.tsx
@@ -0,0 +1,48 @@
+import { useInfiniteQuery } from "@tanstack/react-query";
+
+import { githubApi } from "../../api/githubApi";
+import { sleep } from "../../helpers";
+import { Issue, State } from "../interfaces";
+
+
+interface Props {
+  state?: State,
+  labels: string[],
+  page?: number,
+}
+
+interface QueryProps {
+  pageParam?: number;
+  queryKey: (string | Props)[],
+}
+
+const getIssues = async ({ pageParam = 1, queryKey }: QueryProps):Promise<Issue[]> => {
+  const [,, args] = queryKey;
+  const { state, labels } = args as Props;
+  await sleep(2);
+  const params = new URLSearchParams();
+  if (state) params.append('state', state);
+  if (labels.length > 0) {
+    const labelsString = labels.join(',');
+    params.append('labels', labelsString);
+  }
+  params.append('page', pageParam.toString());
+  params.append('per_page', '5');
+  const { data } = await githubApi.get<Issue[]>('/issues', { params });
+  return data;
+};
+
+export const useIssuesInfinite = ({ state, labels }: Props) => {
+  const issuesQuery = useInfiniteQuery(
+    [ 'issues', 'infinite', { state, labels, page: 1 } ],
+    (data) => getIssues(data),
+    {
+      // TODO
+      // getNextPageParam()
+    },
+  );
+  
+  return {
+    issuesQuery,
+  };
+}
