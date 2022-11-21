8c4e57fb339d69f97f4cf4c3674ab7799ac5f607
Miguel Sanchez <mangelsr25@gmail.com>
2022-11-21 10:58:05 -0500

    [ADD] properties and handler for pagination management

--- src/issues/hooks/useIssues.tsx
+++ src/issues/hooks/useIssues.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
 
 import { githubApi } from '../../api/githubApi';
@@ -8,9 +9,10 @@ import { sleep } from '../../helpers/sleep';
 interface Props {
   state?: State,
   labels: string[],
+  page?: number,
 }
 
-const getIssues = async (labels: string[] = [], state?: State):Promise<Issue[]> => {
+const getIssues = async ({ labels, state, page = 1 }: Props):Promise<Issue[]> => {
   await sleep(2);
   const params = new URLSearchParams();
   if (state) params.append('state', state);
@@ -18,16 +20,38 @@ const getIssues = async (labels: string[] = [], state?: State):Promise<Issue[]> 
     const labelsString = labels.join(',');
     params.append('labels', labelsString);
   }
-  params.append('page', '1');
+  params.append('page', page.toString());
   params.append('per_page', '5');
   const { data } = await githubApi.get<Issue[]>('/issues', { params });
   return data;
 };
 
 export const useIssues = ({ state, labels }: Props) => {
+  const [page, setPage] = useState<number>(1);
+  
   const issuesQuery = useQuery(
-    ['issues', { state, labels }],
-    () => getIssues(labels, state),
+    ['issues', { state, labels, page }],
+    () => getIssues({ labels, state, page }),
   );
-  return { issuesQuery };
+
+  const nextPage = () => {
+    if (issuesQuery.data?.length == 0) return;
+    setPage(page + 1);
+  };
+
+  const prevPage = () => {
+    if (page > 1) setPage(page - 1);
+  };
+
+  return { 
+    // Properties
+    issuesQuery,
+    
+    // Getter
+    page,
+
+    // Methods
+    nextPage,
+    prevPage,
+  };
 };
