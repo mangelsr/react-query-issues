6d60501c31d9b6528534097b2bedc68a13976e4b
Miguel Sanchez <mangelsr25@gmail.com>
2022-11-21 15:32:04 -0500

    [ADD] ListViewInfinite component [ADD] added to views barrel file

---
+++ src/issues/views/ListViewInfinite.tsx
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+
+import { LoadingIcon } from '../../shared/components/LoadingIcon';
+import { IssueList } from '../components/IssueList';
+import { LabelPicker } from '../components/LabelPicker';
+import { useIssuesInfinite } from '../hooks';
+import { State } from '../interfaces';
+
+
+export const ListViewInfinite = () => {
+  
+  const [selectedLabels, setSelectedLabels] = useState<string[]>([]);
+  const [state, setState] = useState<State>();
+  const { issuesQuery } = useIssuesInfinite({state, labels: selectedLabels});
+  
+  const onLabelChanged = (labelName: string) => {
+    selectedLabels.includes(labelName)
+      ? setSelectedLabels(selectedLabels.filter(label => label != labelName))
+      : setSelectedLabels([...selectedLabels, labelName]);
+  };
+  
+  return (
+    <div className="row mt-5">
+      
+      <div className="col-8">
+        { 
+          issuesQuery.isLoading
+            ? <LoadingIcon/>
+            : <IssueList
+                issues={ issuesQuery.data?.pages.flat() || [] }
+                state={ state }
+                onStateChanged={ (newState) => setState(newState) }
+              />
+        }
+        <button className='btn btn-outline-primary mt-2'>Load more...</button>
+      </div>
+      
+      <div className="col-4">
+        <LabelPicker
+          selectedLabels={selectedLabels}
+          onChange={(labelName) => onLabelChanged(labelName)}
+        />
+      </div>
+    </div>
+  )
+}
